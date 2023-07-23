64c3e43dbc87380a4bf3c3ab885c9fa9e7217288
bugloader <36098023+bugloader@users.noreply.github.com>
2023-07-23 06:29:41 -0500

    uno render mixin attampt1

--- src/main/java/com/meacks/table_game/items/MinoHandCard.java
+++ src/main/java/com/meacks/table_game/items/MinoHandCard.java
@@ -1,9 +1,148 @@
 package com.meacks.table_game.items;
 
+import com.meacks.table_game.handlers.BlockHandler;
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MinoHandCard extends Item {
+
     public MinoHandCard(){
         super(new Item.Properties().stacksTo(1));
     }
+    //informational methods
+    public static final String RESOURCE_PATH_ROOT ="table_game:textures/item/mino/uno";
+    public static final String[] COLORS = {"creeper","diamond","ocelot","redstone"};
+    public static final String[] TYPES = {"draw2","reverse","skip"};
+
+    public static final int CARDS_COUNT = 55;
+    public static String getCardName(int id){
+        if(id==0)return "";
+        if(id==1)return "-wild";
+        if(id==2)return "-wild-draw4";
+        int colorId=(id-3)/13;
+        int typeId=(id-3)%13;
+        if(typeId<10) return "-"+ COLORS[colorId]+'-'+Integer.toString(typeId);
+        return "-"+ COLORS[colorId]+'-'+TYPES[typeId-10];
+    }
+
+    public static ArrayList<Integer> basicCardList(){
+        ArrayList<Integer> cardsList = new ArrayList<>();
+        cardsList.add(0);
+        return cardsList;
+    }
+
+    public static CompoundTag basicTag(){
+        CompoundTag nbt = new CompoundTag();
+        nbt.putInt("index",0);
+        nbt.putBoolean("0",true);
+        for (int i = 1; i < CARDS_COUNT; i++) nbt.putBoolean(Integer.toString(i),false);
+        return nbt;
+    }
+
+    public static int getCurrentCardId(ItemStack stack) {
+        ArrayList<Integer> cardsList = basicCardList();
+        CompoundTag nbt = stack.getTag();
+        int id=0;
+        if (nbt != null) {
+            for (int i = 1; i < CARDS_COUNT; i++) if(nbt.getBoolean(Integer.toString(i))) cardsList.add(i);
+            id = nbt.getInt("index");
+        }
+        stack.setTag(nbt);
+        return cardsList.get(id);
+    }
+    public static String getCurrentCardName(ItemStack stack){
+        return getCardName(getCurrentCardId(stack));
+    }
+
+    public static String getCurrentCardPath(ItemStack stack){
+        return RESOURCE_PATH_ROOT+getCurrentCardName(stack)+".png";
+    }
+
+    //functional methods
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> list, TooltipFlag flag) {
+        ArrayList<Integer> cardsList = basicCardList();
+        CompoundTag nbt = stack.getTag();
+        if (nbt == null){
+            nbt=basicTag();
+            stack.setTag(nbt);
+        }
+        int index=nbt.getInt("index");
+        for (int i = 1; i < CARDS_COUNT; i++) if(nbt.getBoolean(Integer.toString(i))) cardsList.add(i);
+        for (int i = 0;i < cardsList.size();i++) {
+            if(i==index) list.add(Component.translatable(getCardName(cardsList.get(i))).withStyle(ChatFormatting.AQUA));
+            else list.add(Component.translatable(getCardName(cardsList.get(i))));
+        }
+        super.appendHoverText(stack,level,list,flag);
+    }
+
+
+    public void switchCard(ItemStack stack,boolean decrease){
+        CompoundTag nbt = stack.getTag();
+        if (nbt == null) nbt = basicTag();
+        else {
+            int currentIndex = nbt.getInt("index");
+            int counter=1;
+            for (int i = 1; i < CARDS_COUNT; i++) if(nbt.getBoolean(Integer.toString(i))) counter++;
+            if(decrease) nbt.putInt("index",(currentIndex+counter-1)%counter);
+            else nbt.putInt("index",(currentIndex+1)%counter);
+        }
+        stack.setTag(nbt);
+        System.out.println("1:"+getCurrentCardId(stack));
+        stack.setHoverName(Component.translatable(getCurrentCardName(stack)));
+    }
+
+
+    //testing methods
+    public void makeFullStackOfCard(ItemStack stack){
+        CompoundTag nbt = stack.getTag();
+        if (nbt == null) nbt = basicTag();
+        for (int i = 1; i < CARDS_COUNT; i++) nbt.putBoolean(Integer.toString(i),true);
+        stack.setTag(nbt);
+    }
+
+
+    @Override
+    public InteractionResult useOn(@NotNull UseOnContext useOnContext) {
+        BlockPos clickedBlockPos = useOnContext.getClickedPos();
+        Level level = useOnContext.getLevel();
+        Block clickedBlock = level.getBlockState(clickedBlockPos).getBlock();
+        Player player = useOnContext.getPlayer();
+        if(level.isClientSide) return InteractionResult.SUCCESS;
+        if(player==null) return InteractionResult.PASS;
+        if(BlockHandler.areSameBlockType(clickedBlock,Blocks.AIR)){
+
+        } else if (BlockHandler.areSameBlockType(clickedBlock,BlockHandler.small_game_table.get())) {
+            makeFullStackOfCard(useOnContext.getItemInHand());
+            System.out.println(2);
+        }
+        return InteractionResult.SUCCESS;
+    }
+    @Override
+    public InteractionResultHolder<ItemStack> use(@NotNull Level level, @NotNull Player player, InteractionHand interactionHand) {
+        ItemStack stack=player.getItemInHand(interactionHand);
+        if(level.isClientSide) return InteractionResultHolder.success(stack);
+        switchCard(stack,player.isShiftKeyDown());
+        System.out.println(1);
+        return InteractionResultHolder.success(stack);
+    }
+
 }
--- src/main/java/com/meacks/table_game/mixin/MixinsItemInHandRenderer.java
+++ src/main/java/com/meacks/table_game/mixin/MixinsItemInHandRenderer.java
@@ -1,17 +1,24 @@
 package com.meacks.table_game.mixin;
 
 import com.meacks.table_game.TableGameMod;
+import com.meacks.table_game.handlers.ItemHandler;
+import com.meacks.table_game.items.MinoHandCard;
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.MapItem;
+import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.HumanoidArm;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
@@ -25,36 +32,130 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ItemInHandRenderer.class)
 public abstract class MixinsItemInHandRenderer {
 
-
     @Final
     @Shadow
     private Minecraft minecraft;
+    @Shadow
+    private ItemStack mainHandItem;
+    @Shadow
+    private ItemStack offHandItem;
+    @Final
+    @Shadow
+    private EntityRenderDispatcher entityRenderDispatcher;
 
-    @Inject(at = @At(value = "HEAD"), method = "Lnet/minecraft/client/renderer/ItemInHandRenderer;renderMap(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/item/ItemStack;)V")
-    private void renderMap(PoseStack p_109367_, MultiBufferSource p_109368_, int p_109369_, ItemStack p_109370_, CallbackInfo ci) {
-        ci.cancel();
-        RenderType MAP_BACKGROUND = RenderType.text(new ResourceLocation("textures/map/map_background.png"));
-        RenderType MAP_BACKGROUND_CHECKERBOARD = RenderType.text(new ResourceLocation("textures/map/map_background_checkerboard.png"));
+    @Shadow
+    private float calculateMapTilt(float p_109313_) {
+        throw new IllegalStateException("Mixin failed to shadow calculateMapTilt()");
+    }
+    @Shadow
+    private void renderPlayerArm(PoseStack p_109347_, MultiBufferSource p_109348_, int p_109349_, float p_109350_,
+                                 float p_109351_, HumanoidArm p_109352_) {
+        throw new IllegalStateException("Mixin failed to shadow renderPlayerArm()");
+    }
 
-        //MAP_BACKGROUND = RenderType.text(new ResourceLocation(TableGameMod.MODID,"textures/map/empty.png"));
-        //MAP_BACKGROUND_CHECKERBOARD = RenderType.text(new ResourceLocation(TableGameMod.MODID,"textures/map/empty.png"));
-
+    //Main implementation
+     private void renderMap(PoseStack p_109367_, MultiBufferSource p_109368_, int p_109369_, ItemStack p_109370_) {
         p_109367_.mulPose(Axis.YP.rotationDegrees(180.0F));
         p_109367_.mulPose(Axis.ZP.rotationDegrees(180.0F));
         p_109367_.scale(0.38F, 0.38F, 0.38F);
         p_109367_.translate(-0.5F, -0.5F, 0.0F);
         p_109367_.scale(0.0078125F, 0.0078125F, 0.0078125F);
-        Integer integer = MapItem.getMapId(p_109370_);
-        System.out.println(1);
-        MapItemSavedData mapitemsaveddata = MapItem.getSavedData(integer, this.minecraft.level);
-        VertexConsumer vertexconsumer = p_109368_.getBuffer(mapitemsaveddata == null ? MAP_BACKGROUND : MAP_BACKGROUND_CHECKERBOARD);
+        VertexConsumer vertexconsumer = p_109368_.getBuffer(RenderType.text(
+                new ResourceLocation(MinoHandCard.getCurrentCardPath(p_109370_))));
         Matrix4f matrix4f = p_109367_.last().pose();
-        vertexconsumer.vertex(matrix4f, -7.0F, 135.0F, 0.0F).color(255, 255, 255, 255).uv(0.0F, 1.0F).uv2(p_109369_).endVertex();
-        vertexconsumer.vertex(matrix4f, 135.0F, 135.0F, 0.0F).color(255, 255, 255, 255).uv(1.0F, 1.0F).uv2(p_109369_).endVertex();
-        vertexconsumer.vertex(matrix4f, 135.0F, -7.0F, 0.0F).color(255, 255, 255, 255).uv(1.0F, 0.0F).uv2(p_109369_).endVertex();
-        vertexconsumer.vertex(matrix4f, -7.0F, -7.0F, 0.0F).color(255, 255, 255, 255).uv(0.0F, 0.0F).uv2(p_109369_).endVertex();
-        if (mapitemsaveddata != null) {
-            this.minecraft.gameRenderer.getMapRenderer().render(p_109367_, p_109368_, integer, mapitemsaveddata, false, p_109369_);
+        vertexconsumer.vertex(matrix4f, -7.0F, 135.0F, 0.0F).color(255,
+                255, 255, 255).uv(0.0F, 1.0F).uv2(p_109369_).endVertex();
+        vertexconsumer.vertex(matrix4f, 135.0F, 135.0F, 0.0F).color(255,
+                255, 255, 255).uv(1.0F, 1.0F).uv2(p_109369_).endVertex();
+        vertexconsumer.vertex(matrix4f, 135.0F, -7.0F, 0.0F).color(255,
+                255, 255, 255).uv(1.0F, 0.0F).uv2(p_109369_).endVertex();
+        vertexconsumer.vertex(matrix4f, -7.0F, -7.0F, 0.0F).color(255,
+                255, 255, 255).uv(0.0F, 0.0F).uv2(p_109369_).endVertex();
+    }
+
+
+    private void renderMapHand(PoseStack p_109362_, MultiBufferSource p_109363_, int p_109364_, HumanoidArm p_109365_) {
+        RenderSystem.setShaderTexture(0, this.minecraft.player.getSkinTextureLocation());
+        PlayerRenderer playerrenderer = (PlayerRenderer)
+                this.entityRenderDispatcher.<AbstractClientPlayer>getRenderer(this.minecraft.player);
+        p_109362_.pushPose();
+        float f = p_109365_ == HumanoidArm.RIGHT ? 1.0F : -1.0F;
+        p_109362_.mulPose(Axis.YP.rotationDegrees(92.0F));
+        p_109362_.mulPose(Axis.XP.rotationDegrees(45.0F));
+        p_109362_.mulPose(Axis.ZP.rotationDegrees(f * -41.0F));
+        p_109362_.translate(f * 0.3F, -1.1F, 0.45F);
+        if (p_109365_ == HumanoidArm.RIGHT) {
+            playerrenderer.renderRightHand(p_109362_, p_109363_, p_109364_, this.minecraft.player);
+        } else {
+            playerrenderer.renderLeftHand(p_109362_, p_109363_, p_109364_, this.minecraft.player);
+        }
+
+        p_109362_.popPose();
+    }
+
+    private void renderOneHandedMap(PoseStack p_109354_, MultiBufferSource p_109355_, int p_109356_, float p_109357_,
+                                    HumanoidArm p_109358_, float p_109359_, ItemStack p_109360_) {
+        float f = p_109358_ == HumanoidArm.RIGHT ? 1.0F : -1.0F;
+        p_109354_.translate(f * 0.125F, -0.125F, 0.0F);
+        if (!this.minecraft.player.isInvisible()) {
+            p_109354_.pushPose();
+            p_109354_.mulPose(Axis.ZP.rotationDegrees(f * 10.0F));
+            this.renderPlayerArm(p_109354_, p_109355_, p_109356_, p_109357_, p_109359_, p_109358_);
+            p_109354_.popPose();
+        }
+
+        p_109354_.pushPose();
+        p_109354_.translate(f * 0.51F, -0.08F + p_109357_ * -1.2F, -0.75F);
+        float f1 = Mth.sqrt(p_109359_);
+        float f2 = Mth.sin(f1 * (float)Math.PI);
+        float f3 = -0.5F * f2;
+        float f4 = 0.4F * Mth.sin(f1 * ((float)Math.PI * 2F));
+        float f5 = -0.3F * Mth.sin(p_109359_ * (float)Math.PI);
+        p_109354_.translate(f * f3, f4 - 0.3F * f2, f5);
+        p_109354_.mulPose(Axis.XP.rotationDegrees(f2 * -45.0F));
+        p_109354_.mulPose(Axis.YP.rotationDegrees(f * f2 * -30.0F));
+        this.renderMap(p_109354_, p_109355_, p_109356_, p_109360_);
+        p_109354_.popPose();
+    }
+    private void renderTwoHandedMap(PoseStack p_109340_, MultiBufferSource p_109341_, int p_109342_, float p_109343_,
+                                    float p_109344_, float p_109345_) {
+        float f = Mth.sqrt(p_109345_);
+        float f1 = -0.2F * Mth.sin(p_109345_ * (float)Math.PI);
+        float f2 = -0.4F * Mth.sin(f * (float)Math.PI);
+        p_109340_.translate(0.0F, -f1 / 2.0F, f2);
+        float f3 = this.calculateMapTilt(p_109343_);
+        p_109340_.translate(0.0F, 0.04F + p_109344_ * -1.2F + f3 * -0.5F, -0.72F);
+        p_109340_.mulPose(Axis.XP.rotationDegrees(f3 * -85.0F));
+        if (!this.minecraft.player.isInvisible()) {
+            p_109340_.pushPose();
+            p_109340_.mulPose(Axis.YP.rotationDegrees(90.0F));
+            this.renderMapHand(p_109340_, p_109341_, p_109342_, HumanoidArm.RIGHT);
+            this.renderMapHand(p_109340_, p_109341_, p_109342_, HumanoidArm.LEFT);
+            p_109340_.popPose();
+        }
+
+        float f4 = Mth.sin(f * (float)Math.PI);
+        p_109340_.mulPose(Axis.XP.rotationDegrees(f4 * 20.0F));
+        p_109340_.scale(2.0F, 2.0F, 2.0F);
+        this.renderMap(p_109340_, p_109341_, p_109342_, this.mainHandItem);
+    }
+
+
+    @Inject(at = @At(value = "HEAD"), method = "Lnet/minecraft/client/renderer/ItemInHandRenderer;renderArmWithItem(Lnet/minecraft/client/player/AbstractClientPlayer;FFLnet/minecraft/world/InteractionHand;FLnet/minecraft/world/item/ItemStack;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", cancellable = true)
+    private void renderArmWithItem(AbstractClientPlayer p_109372_, float p_109373_, float p_109374_,
+                                   InteractionHand p_109375_, float p_109376_, ItemStack p_109377_, float p_109378_,
+                                   PoseStack p_109379_, MultiBufferSource p_109380_, int p_109381_, CallbackInfo ci) {
+        if (!p_109372_.isScoping()) {
+            boolean flag = p_109375_ == InteractionHand.MAIN_HAND;
+            HumanoidArm humanoidarm = flag ? p_109372_.getMainArm() : p_109372_.getMainArm().getOpposite();
+            if (p_109377_.is(ItemHandler.mino_hand_card.get())) {
+                if (flag && this.offHandItem.isEmpty()) {
+                    this.renderTwoHandedMap(p_109379_, p_109380_, p_109381_, p_109374_, p_109378_, p_109376_);
+                } else {
+                    this.renderOneHandedMap(p_109379_, p_109380_, p_109381_, p_109378_, humanoidarm, p_109376_, p_109377_);
+                }
+                ci.cancel();
+            }
         }
     }
 }
